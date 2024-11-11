e0558f4f6accdaffb8e8c75f162e82cb7d39fdaf
Mariana Trinko <marjanavv@gmail.com>
2024-11-11 17:56:14 +0100

    message

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import { Loader } from './components/Loader';
-
 import './App.scss';
 import { NavLink, Outlet } from 'react-router-dom';
 import classNames from 'classnames';
 
 export const App = () => {
   const getLinkClass = ({ isActive }: { isActive: boolean }) =>
-    classNames('navbar-item', {'has-background-grey-lighter': isActive });
+    classNames('navbar-item', { 'has-background-grey-lighter': isActive });
 
   return (
     <div data-cy="app">
@@ -22,10 +20,7 @@ export const App = () => {
               Home
             </NavLink>
 
-            <NavLink
-              className={getLinkClass}
-              to="/people"
-            >
+            <NavLink className={getLinkClass} to="/people">
               People
             </NavLink>
           </div>
@@ -34,23 +29,13 @@ export const App = () => {
 
       <main className="section">
         <div className="container">
-
-
           <div className="block">
             <div className="box table-container">
-              {/* <Loader /> */}
-
-              {/* <p data-cy="peopleLoadingError" className="has-text-danger">
-                Something went wrong
-              </p>
-
-              <p data-cy="noPeopleMessage">There are no people on the server</p> */}
-
-<Outlet/>
+              <Outlet />
             </div>
           </div>
         </div>
       </main>
     </div>
   );
-}
+};
